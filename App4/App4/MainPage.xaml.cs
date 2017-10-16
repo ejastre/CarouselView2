c08fe9056e0f27efa3beec488ee7889622ca8849
ejastre <edgar.jastre@gmail.com>
2017-10-16 20:51:27 +0100

    commit

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Zoo> Zoos { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Zoos = new ObservableCollection<Zoo>
+            {
+                new Zoo
+                {
+                    ImageUrl = "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/23c1dd13-333a-459e-9e23-c3784e7cb434/2016-06-02_1049.png",
+                    Name = "Woodland Park Zoo"
+                },
+                new Zoo
+                {
+                    ImageUrl =    "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/6b60d27e-c1ec-4fe6-bebe-7386d545bb62/2016-06-02_1051.png",
+                    Name = "Cleveland Zoo"
+                },
+                new Zoo
+                {
+                    ImageUrl = "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/e8179889-8189-4acb-bac5-812611199a03/2016-06-02_1053.png",
+                    Name = "Phoenix Zoo"
+                }
+            };
+
+            CarouselZoos.ItemsSource = Zoos;
+            CarouselZoos2.ItemsSource = Zoos;
+        }
+
+        public class Zoo
+        {
+            public string ImageUrl { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
